5e3e427b78577327d22ea6af4a8484220e447154
tidalga <amazingachoo@gmail.com>
2026-03-12 01:20:40 -0700

    feat: Started beatmap Timings of beatmap based on note duration (ex. 1/4 note) First 15ish seconds of beatmap completed - Hitsound added - Still need to fix ending

--- rhythm/scripts/sketch.js
+++ rhythm/scripts/sketch.js
@@ -1,23 +1,23 @@
 /*
-NOTE: This project contains other JavaScript files within [scripts/]
+NOTE: This project contains other JavaScript files within 'scripts/'
 Other additions present in index.html and style.css
 */
 
 /*==========[q5.js FUNCTIONS]==========*/
 function preload(){
     beatmaps[beatmaps.currMap].audio = loadSound(beatmaps[beatmaps.currMap].audiopath);
+    beatmaps.hitsound = loadSound("/rhythm/audios/soft-hitnormal.wav");
     beatmaps[beatmaps.currMap].audio.volume = 0.125;
 }
 
 function setup(){
     createCanvas(canvasWidth, canvasHeight);
     canvasSetup();
-    domVarsSetup();
-    circleSetup();
     noLoop();
 }
 
 function keyPressed(){
+    beatmaps.hitsound.play();
     let time = calcTime();
     let pos = calcHitPos(mouseX, mouseY);
     registerHit(time, pos);
